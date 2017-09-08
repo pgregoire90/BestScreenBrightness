56875f383168cdfbbde3b0a64fc499fb1b0a2244
pgregoire90 <pasgreg@hotmail.com>
2017-09-08 11:43:03 -0400

    Added next image feature

--- app/src/main/java/com/example/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -1,25 +1,41 @@
 package com.example.myapplicationtest;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE ="com.example.myapplicationtest.MESSAGE" ;
+    private static final String LOG_TAG ="Empty";
     SeekBar seekBar_bottom;
     SeekBar seekBar_bright;
     SeekBar seekBar_top;
+    int curSlide = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
 
         seekBar_bottom=(SeekBar)findViewById(R.id.seekBar2);
 
@@ -105,13 +121,24 @@ public class MainActivity extends AppCompatActivity {
         // getWindow().setAttributes(layout);
     }
 
+    private static int[] imgs = { R.drawable.images_test, R.drawable.image2};
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+
+
+    public void nextImage(View view) throws IOException {
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+        if (curSlide<imgs.length) {
+            ImageView img = (ImageView) findViewById(R.id.image);
+            img.setImageResource(imgs[curSlide]);
+            curSlide = curSlide + 1;
+        }
+
+
     }
 
 }
